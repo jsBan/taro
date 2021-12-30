c49b81b122381062dc75dc8c64bccb1cc26fb7aa
banguoxiang <banguoxiang>
2021-12-30 16:26:44 +0800

    fix: nav和block模块

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -12,17 +12,19 @@ export default function NavBar(props) {
     }
     return (
         <View className='navbarWrap' style={style}>
-            <View className='navbar'>
-                <View className="logo">
-                    <Image src="https://storage.360buyimg.com/mtd/home/111543234387022.jpg"  />
+            <View className="warpper">
+                <View className='navbar'>
+                    <View className="logo">
+                        <Image src="https://storage.360buyimg.com/mtd/home/111543234387022.jpg"  />
+                    </View>
+                    <View className="desc">
+                        <View className="black">只做原装正品二手机</View> 
+                        <View className="gray">原装<Text className="spot">.</Text>正品<Text className="spot">.</Text>一年质保</View>
+                    </View>
                 </View>
-                <View className="desc">
-                    <View className="black">只做原装正品二手机</View> 
-                    <View className="gray">原装<Text className="spot">.</Text>正品<Text className="spot">.</Text>一年质保</View>
+                <View>
+                <Search />
                 </View>
-            </View>
-            <View>
-            <Search />
             </View>
         </View>
     )
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,13 +1,117 @@
 import Taro from '@tarojs/taro'
-import { View } from "@tarojs/components"
+import { View, Swiper, SwiperItem, Image, Text } from "@tarojs/components"
 import { NavBar } from "../../components"
+import { useState } from 'react'
 
 import "./index.less"
 
-const Index =() => {
+const Index = () => {
+    const state = {
+        list: [
+            'https://storage.360buyimg.com/mtd/home/111543234387022.jpg',
+            'https://storage.360buyimg.com/mtd/home/221543234387016.jpg',
+            'https://storage.360buyimg.com/mtd/home/331543234387025.jpg'
+        ],
+        nav:  [
+            {
+            url: require("../../access/images/IPHONE.png"),
+            name: "iPhone",
+           },
+           {
+             url: require("../../access/images/anzhuo.png"),
+             name: "安卓手机",
+           },
+           {
+             url: require("../../access/images/pingban.png"),
+             name: "平板电脑",
+           },
+           {
+             url: require("../../access/images/diannao.png"),
+             name: "电脑",
+           },
+           {
+             url: require("../../access/images/erji.png"),
+             name: "配件",
+           },
+           {
+             url: require("../../access/images/money.png"),
+             name: "捡漏专区",
+           },
+           {
+             url: require("../../access/images/quan.png"),
+             name: "抢优惠券",
+           },
+           {
+             url: require("../../access/images/new.png"),
+             name: "新品推荐",
+           },
+       ],
+    }
     return (
         <View className="wrapper">
             <NavBar />
+            <View className="banner">
+                <Swiper
+                  className="swiper-container"
+                  circular
+                  indicatorDots
+                  indicatorColor='#999'
+                  indicatorActiveColor='#bf708f'
+                  autoplay
+                >
+                    {
+                        state.list && state.list.map((item, index) => (
+                            <SwiperItem key={index}>
+                                <Image className="swiper-img" src={item} mode='widthFix'></Image>
+                            </SwiperItem>
+                        ))
+                    }
+                </Swiper>
+            </View>
+            <View className="nav"> 
+            {
+                state.nav.map((item, index) => (
+                    <View key={index} className="nav-info">
+                        <View className="nav-image">
+                            <Image src={item.url}></Image>
+                            <View className="desc">{item.name}</View>
+                        </View>
+                    </View>
+                ))
+            }
+            </View>
+            <View className="block">
+                <View className="block-left">
+                    <View className="left-title">正品回收.更快.更高价</View>
+                    <View className="left-bottom">
+                        <View className="bottom-left">
+                            <View><Text className="at-icon at-icon-check-circle"></Text>高价回收 加价收</View>
+                            <View><Text className="at-icon at-icon-check-circle"></Text>顺丰上门 安全私密</View>
+                            <View><Text className="at-icon at-icon-check-circle"></Text>专业质检 不卖秒退</View>
+                            <View className="go">
+                                立即前往<Text className="at-icon at-icon-chevron-right"></Text>
+                            </View>
+                        </View>
+                        <View className="bottom-right">
+                            <Image src={state.nav[0].url}></Image>
+                        </View>
+                    </View>
+                </View>
+                <View className="block-right">
+                    <View className="right-title">本机回收</View>
+                    <View className="right-center">还可以领取5%的红包</View>
+                    <View className="right-bottom">
+                        <View className="bottom-left">
+                            <Image src={state.nav[0].url}></Image>
+                        </View>
+                        <View className="bottom-right">
+                            <View>本机iPhone xs max</View>
+                            <View className="senced"><Text className="mark">￥</Text><Text className="money">3300</Text></View>
+                            <View>有550选择卖出</View>
+                        </View>
+                    </View>
+                </View>
+            </View>
         </View>
     )
 }
